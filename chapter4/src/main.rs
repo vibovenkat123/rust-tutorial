583d756ddaa86a2f00e858c90e2f61676ce69d7f
vibovenkat123 <vibovenkat@gmail.com>
2023-03-04 19:09:19 -0600

    feat: add scope tutorial

---
+++ chapter4/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // scope
+    { // start of scope
+       let s = "s"; // can use s from now on
+       println!("{}", s);
+    }
+    // s is not valid anymore
+    // the String type
+    // can be mutable
+    let mut s = String::from("s");
+    s.push_str(" is mutable");
+    println!("{}", s);
+    // you can deallocate a variable
+    drop(s);
+    // error thrown
+    //println!("{}", s);
+}
